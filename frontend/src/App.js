465c40b3d64c5dc99381f542e78634fd7af352de
Indrajith <indrajithss0471@gmail.com>
2023-01-14 07:45:55 +0530

    products view integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { themeSettings } from "./theme"
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./views/layout"
 import Dashboard from "./views/dashboard"
+import Products from "./views/products"
 
 function App() {
   // @ts-ignore
@@ -21,6 +22,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- frontend/src/state/api.js
+++ frontend/src/state/api.js
@@ -7,13 +7,17 @@ export const api = createApi({
   // @ts-ignore
   baseQuery: fetchBaseQuery({ baseUrl: URL }),
   reducerPath: "AdminApi",
-  tagTypes: ["User"],
+  tagTypes: ["User", "Products"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
   }),
 })
 
-export const { useGetUserQuery } = api
+export const { useGetUserQuery, useGetProductsQuery } = api
