ad3ff40c2aa2b249ea591e75802a57999aae182a
Indrajith <indrajithss0471@gmail.com>
2023-01-14 08:19:17 +0530

    getCustomers Routes and Controllers created in backend

--- backend/controllers/client.js
+++ backend/controllers/client.js
@@ -1,6 +1,9 @@
 import Product from "../models/Product.js"
 import ProductStat from "../models/ProductStat.js"
+import Transaction from "../models/Transaction.js"
+import User from "../models/User.js"
 
+// Get Products
 export const getProducts = async (req, res) => {
   try {
     const products = await Product.find()
@@ -18,6 +21,54 @@ export const getProducts = async (req, res) => {
       })
     )
     res.status(200).json(productsWithStats)
+  } catch (e) {
+    res.status(404).json({ message: e.message })
+  }
+}
+
+// Get Customers
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password")
+    res.status(200).json(customers)
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+}
+
+// Get Transactions
+export const getTransactions = async (req, res) => {
+  try {
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query
+
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort)
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      }
+      return sortFormatted
+    }
+    const sortFormatted = Boolean(sort) ? generateSort() : {}
+
+    const transactions = await Transaction.find({
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+      ],
+    })
+      // @ts-ignore
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize)
+
+    const total = await Transaction.countDocuments({
+      name: { $regex: search, $options: "i" },
+    })
+
+    res.status(200).json({
+      transactions,
+      total,
+    })
   } catch (e) {
     res.status(404).json({ message: e.message })
   }
--- backend/routes/client.js
+++ backend/routes/client.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getProducts } from "../controllers/client.js"
+import { getProducts, getCustomers } from "../controllers/client.js"
 
 const router = Router()
 
 router.get("/products", getProducts)
+router.get("/customers", getCustomers)
 
 export default router
